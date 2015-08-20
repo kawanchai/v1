d956eec520a31db44f1bd6f91bf77bbdfefe0433
Aaike Van Roekeghem <aaikevr@gmail.com>
2015-08-20 19:55:49 +0700

    Test Jquery

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -1,5 +1,16 @@
 import {bindable} from 'aurelia-framework';
+import $ from 'jquery';
 
 export class NavBar {
   @bindable router = null;
+
+  click(name){
+
+    
+    $('html, body').animate({
+      scrollTop: $('#'+name).offset().top - 70
+    },400);
+    
+  }
+  
 }
